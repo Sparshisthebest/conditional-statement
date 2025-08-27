7110b8c223f8e0fcd55a5c3fd556ea7ea58ee784
Sparshisthebest <sparsh.nayak0812@gmail.com>
2025-08-27 11:49:59 +0100

    Add files via upload

---
+++ activity3_4.py
@@ -0,0 +1,9 @@
+#checking the number greater or smaller
+s=int(input("enter the number : "))
+if (s<15): 
+    print("s is smaller than 15")
+    print("i am in if block")
+else:
+    print("s is greater than 15")
+    print("i am in else block")
+print("i am not in if and not in else block")
