004f96622e8989abfd35faddb117a2451f242328
Sparshisthebest <sparsh.nayak0812@gmail.com>
2025-08-27 11:23:28 +0100

    Add files via upload

---
+++ activity1_4.py
@@ -0,0 +1,10 @@
+#conditional statement
+#write a program to check if the number is positive
+
+num=-5
+if num > 0:
+    print(num,"is a positive number")
+
+num=-1
+if num > 0:
+    print(num,"is a positive number")
